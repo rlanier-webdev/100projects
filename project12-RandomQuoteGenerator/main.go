b2e5e21522a13478b0aeb951c11a2173d13da7fe
RaShunda Lanier <developer@rashundawilliams.com>
2024-05-02 17:10:38 -0500

    Refactor: Added getRandomQuoteIndex function and improved error handling
    - Added getRandomQuoteIndex function to generate a random quote index within the range of available quotes.
    - Improved error handling in getRandomQuoteIndex to return an error if no quotes are available.
    - Updated main function to use getRandomQuoteIndex to retrieve a random quote index and access the quote accordingly.

--- project12-RandomQuoteGenerator/main.go
+++ project12-RandomQuoteGenerator/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 )
 
 // Quote struct represents a quote with ID, text, and author
@@ -38,6 +39,16 @@ func PopulateQuotes(text, authors []string) []Quote {
 	return quotes
 }
 
+func getRandomQuoteIndex(numQuotes int) (int, error) {
+	if numQuotes == 0 {
+		return -1, fmt.Errorf("Error: No quotes available")
+	}
+	// Generate a random number between 0 and numQuotes
+	randomIndex := rand.Intn(numQuotes)
+
+	return randomIndex, nil
+}
+
 func main() {
 	// Reset lastQuoteID to 0 at the beginning of the program
 	lastQuoteID = 0
@@ -53,6 +64,21 @@ func main() {
 	fmt.Println("Quotes:")
 	for _, quote := range quotes {
 		fmt.Printf("ID: %d, Text: %s, Author: %s\n", quote.ID, quote.Text, quote.Author)
-
 	}
+
+	// Get the number of quotes
+	numQuotes := len(quotes)
+
+	// Generate a random quote index
+	randomIndex, err := getRandomQuoteIndex(numQuotes)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	// Access the random quote using the index
+	randomQuote := quotes[randomIndex]
+
+	fmt.Println("\nRandom Quote:")
+	fmt.Printf("ID: %d, Text: %s, Author: %s\n", randomQuote.ID, randomQuote.Text, randomQuote.Author)
 }
